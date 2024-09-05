4defe735a3385a2a009458d4119234c91c78dc88
Ryan Pimiskern <ryan.pimiskern@ec.gc.ca>
2024-09-05 08:30:20 -0600

    radar product switching now works

--- src/components/ui/RadarSelector.tsx
+++ src/components/ui/RadarSelector.tsx
@@ -1,12 +1,27 @@
-import { RadioGroup, RadioGroupItem } from "./radio-group";
+import Button from "./Button";
+
 import { Switch } from "./switch";
 import { useGeoMetContext } from "@/contexts/geometContext";
 
 const RadarSelector = () => {
   const radar = useGeoMetContext();
 
+  const changeRadarMode = (mode: string) => {
+    switch (mode) {
+      case "rain":
+        radar.setRadarProduct!("RRAI");
+        break;
+      case "snow":
+        radar.setRadarProduct!("RSNO");
+        break;
+      default:
+        console.log("no radar mode selected, somehow");
+        return;
+    }
+  };
+
   return (
-    <div className="flex flex-row justify-between">
+    <div className="flex flex-row justify-around">
       <div>
         <Switch
           className="data-[state=checked]:bg-teal-700"
@@ -18,24 +33,24 @@ const RadarSelector = () => {
           Show Radar
         </label>
       </div>
-      <RadioGroup defaultValue="RADAR_1KM_RRAI">
-        <div className="">
-          <RadioGroupItem
-            value={"RADAR_1KM_RRAI"}
-            id="rain"
-            className="bg-teal-700"
-          />
-          <label htmlFor="rain">Rain</label>
-        </div>
-        <div className="">
-          <RadioGroupItem
-            value={"RADAR_1KM_RSNO"}
-            id="snow"
-            className="bg-teal-700"
-          />
-          <label htmlFor="snow">Snow</label>
-        </div>
-      </RadioGroup>
+      <div className="flex">
+        <Button
+          onClick={() => {
+            changeRadarMode("rain");
+          }}
+          className="flex items-center justify-center rounded-none border border-black bg-teal-700 text-white first-of-type:rounded-s-md last-of-type:rounded-e-md hover:cursor-pointer hover:bg-teal-500"
+        >
+          Rain
+        </Button>
+        <Button
+          onClick={() => {
+            changeRadarMode("snow");
+          }}
+          className="flex items-center justify-center rounded-none border border-black bg-teal-700 text-white first-of-type:rounded-s-md last-of-type:rounded-e-md hover:cursor-pointer hover:bg-teal-500"
+        >
+          Snow
+        </Button>
+      </div>
     </div>
   );
 };
