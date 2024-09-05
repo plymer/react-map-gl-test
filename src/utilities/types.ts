ebde6b2e58e7cfb61707951851def0fa7c7ecc1e
Ryan Pimiskern <ryan.pimiskern@ec.gc.ca>
2024-09-05 14:44:58 -0600

    AnimationControls deprecated

--- src/utilities/types.ts
+++ src/utilities/types.ts
@@ -1,11 +1,9 @@
 export type View = { lon: number; lat: number; zoom: number };
 
 export type LayerDetails = {
-    timeStart: number;
-    timeEnd: number;
-    timeSlices: number;
-    timeDiff: number;
-    urls: string[];
+  timeStart: number;
+  timeEnd: number;
+  timeSlices: number;
+  timeDiff: number;
+  urls: string[];
 };
-
-export type AnimationControls = "last" | "prev" | "play-pause" | "next" | "first";
