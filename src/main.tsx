96629efd942f51d8eb59d69e5fe29a37378de6cb
Ryan Pimiskern <ryan.pimiskern@ec.gc.ca>
2024-09-05 18:28:51 -0600

    disable reactquery devtools again

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App.tsx";
 import "./index.css";
 import { MapProvider } from "react-map-gl";
@@ -19,7 +19,7 @@ createRoot(document.getElementById("root")!).render(
             <App />
           </AnimationContextProvider>
         </GeoMetContextProvider>
-        <ReactQueryDevtools />
+        {/* <ReactQueryDevtools /> */}
       </MapProvider>
     </QueryClientProvider>
   </StrictMode>,
